4ffbe851eb3d2ea54049ad34a35451da21c80470
IronKim <maxboystar@naver.com>
2015-11-03 00:22:25 +0900

    2015-11-03 00:22 try bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//MongoDB
+var flash = require('connect-flash');
+var mongoose   = require('mongoose');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,13 +18,26 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//???
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+app.locals.moment = require('moment');
+
+// mongodb connect
+mongoose.connect('mongodb://IronKim:!fpdrk13@ds049094.mongolab.com:49094/survey');
+mongoose.connection.on('error', console.log);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(methodOverride('_method', {methods: ['POST', 'GET']}));       //????
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
 
 app.use('/', routes);
 app.use('/users', users);
